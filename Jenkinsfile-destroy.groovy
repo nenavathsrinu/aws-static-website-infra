d57f2b61940c4dbbc971dc28463b7dc58d05b74b
srinu <nenavath2013@gmail.com>
2025-08-07 20:53:09 +0530

    terraform0

--- Jenkinsfile-destroy.groovy
+++ Jenkinsfile-destroy.groovy
@@ -3,7 +3,11 @@ pipeline {
 
     parameters {
         choice(name: 'ENVIRONMENT', choices: ['dev', 'stg', 'prod'], description: 'Select environment')
-        choice(name: 'AWS_REGION', choices: ['us-east-1', 'us-west-1', 'ap-south-1'], description: 'Select AWS region')
+        choice(name: 'AWS_REGION', choices: [
+            'us-east-1', 'us-west-1', 'us-west-2',
+            'eu-west-1', 'eu-central-1',
+            'ap-south-1', 'ap-northeast-1'
+        ], description: 'Select AWS region')
     }
 
     environment {
@@ -12,24 +16,42 @@ pipeline {
     }
 
     stages {
-        stage('Checkout Code') {
+        stage('Checkout') {
             steps {
                 git url: 'https://github.com/nenavathsrinu/aws-static-website-infra.git', branch: 'main'
             }
         }
 
-        stage('Terraform Destroy') {
+        stage('Terraform Init') {
             steps {
-                withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'aws-credentials']]) {
-                    dir("envs/${params.ENVIRONMENT}") {
-                        bat """
-                        terraform init
-                        terraform validate
-                        terraform destroy -auto-approve -var="environment=%TF_VAR_environment%" -var="region=%TF_VAR_region%" -var-file="terraform.tfvars"
-                        """
+                withAWS(credentials: 'aws-credentials') {
+                    dir("${WORKSPACE}") {
+                        bat "terraform init -backend-config=envs\\\\${params.ENVIRONMENT}\\\\backend.tfvars"
                     }
                 }
             }
+        }
+
+        stage('Terraform Validate') {
+            steps {
+                bat 'terraform validate'
+            }
+        }
+
+        stage('Terraform Destroy') {
+            steps {
+                withAWS(credentials: 'aws-credentials') {
+                    dir("${WORKSPACE}") {
+                        bat 'terraform destroy -auto-approve -var="environment=%TF_VAR_environment%" -var="region=%TF_VAR_region%" -var-file="envs\\\\%TF_VAR_environment%\\\\terraform.tfvars"'
+                    }
+                }
+            }
+        }
+    }
+
+    post {
+        always {
+            echo "⚠️ Terraform destroy completed for environment: '${params.ENVIRONMENT}' in region: '${params.AWS_REGION}'"
         }
     }
 }
