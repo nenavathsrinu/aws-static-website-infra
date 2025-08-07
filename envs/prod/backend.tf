1095387680b4630229e302bfad36d5fa5478d2b1
srinu <nenavath2013@gmail.com>
2025-08-07 13:45:43 +0530

    terraform

--- envs/prod/backend.tf
+++ envs/prod/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "teerafor-state-files-by-project "
-    key            = "state/${var.env}/${var.region}/terraform.tfstate"
-    region         = var.region
+    bucket         = "teerafor-state-files-by-project"
+    key            = "state/prod/us-east-1/terraform.tfstate"
+    region         = "us-east-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
   }
