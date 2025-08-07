9f650f2fe05425d9a7a698138555035180f48acc
srinu <nenavath2013@gmail.com>
2025-08-07 20:07:21 +0530

    terraform

--- Jenkinsfile-apply.groovy
+++ Jenkinsfile-apply.groovy
@@ -2,11 +2,9 @@ pipeline {
     agent any
 
     parameters {
-        string(name: 'ENV', description: 'Environment (prod/stg/uat)')
-    }
-
-    environment {
-        TF_VAR_environment = "${params.ENV}"
+        string(name: 'ENV', defaultValue: '', description: 'Environment (e.g. prod, stg, dev)')
+        string(name: 'REGION', defaultValue: '', description: 'AWS region')
+        choice(name: 'ACTION', choices: ['plan', 'apply'], description: 'Terraform action')
     }
 
     stages {
@@ -14,7 +12,7 @@ pipeline {
             steps {
                 dir("${env.WORKSPACE}") {
                     bat """
-                    terraform init -backend-config="envs/${TF_VAR_environment}/backend.tfvars"
+                    terraform init -backend-config="envs/%ENV%/backend.tfvars"
                     """
                 }
             }
@@ -23,28 +21,32 @@ pipeline {
         stage('Terraform Validate') {
             steps {
                 dir("${env.WORKSPACE}") {
-                    bat """
-                    terraform validate
-                    """
+                    bat "terraform validate"
                 }
             }
         }
 
         stage('Terraform Plan') {
+            when {
+                expression { return params.ACTION == 'plan' || params.ACTION == 'apply' }
+            }
             steps {
                 dir("${env.WORKSPACE}") {
                     bat """
-                    terraform plan -var-file="envs/${TF_VAR_environment}/terraform.tfvars"
+                    terraform plan -var="environment=%ENV%" -var="region=%REGION%" -var-file="envs/%ENV%/terraform.tfvars"
                     """
                 }
             }
         }
 
         stage('Terraform Apply') {
+            when {
+                expression { return params.ACTION == 'apply' }
+            }
             steps {
                 dir("${env.WORKSPACE}") {
                     bat """
-                    terraform apply -auto-approve -var-file="envs/${TF_VAR_environment}/terraform.tfvars"
+                    terraform apply -auto-approve -var="environment=%ENV%" -var="region=%REGION%" -var-file="envs/%ENV%/terraform.tfvars"
                     """
                 }
             }
